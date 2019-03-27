3317aa140bfb8e7923f6ab591b38d9f5e89a521a
jasonasav <jasonasav@gmail.com>
2019-03-27 14:27:13 -0700

    build up the slider component files. added the slide component, left and right arrow components. All components are connected on slider class component.

---
+++ Client/src/Slide.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Slide = () => {
+    return <div className="slide"></div>
+}
+
+
+export default Slide
---
+++ Client/src/Slider.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Axios from 'axios';
+
+
+class Slider extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+
+    }
+
+    render () {
+        return (
+            <div className="slider">
+
+            </div>
+        );
+    }
+    
+    
+}
+
+export default Slider;
